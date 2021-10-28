04ce351c1b478a2cf6efb39f5e8702980f02eef0
EdsonFer <edlivelifehd1z@gmail.com>
2021-10-28 09:58:17 -0300

    first commit

---
+++ src/components/Profile.tsx
@@ -0,0 +1,16 @@
+import styles from '../styles/components/Profile.module.css';
+
+export const Profile = () => {
+	return (
+		<div className={styles.profileContainer}>
+			<img src="https://github.com/EdsonFer.png" alt="Edson Fernandes" />
+			<div>
+				<strong>Edson Fernandes</strong>
+				<p>
+					<img src="icons/level.svg" alt="level" />
+					Level 1
+				</p>
+			</div>
+		</div>
+	);
+};
